8ab395d1c95aa7560f89685c775354e239ef7571
Vaibhav <faichi@Suryakants-MacBook-Pro.local>
2014-10-08 07:18:40 +0530

    new changed

---
+++ Assets/Scripts/CamMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+//		this.gameObject.GetComponent<iTweenEvent> ().tweenName = ""
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+		if (GUI.Button (new Rect (Screen.width - 250, Screen.height - 250, 200, 100), "GO")) {
+			iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("StartPath1"), "easetype", "linear", "time",25, "islocal",true, "orientToPath",true, "lookTime", 0.2, "loopType", "none", "oncomplete", "cde"));
+		}
+	}
+
+	public void abc () {
+		Debug.Log ("Vaibhv");
+	}
+
+	public void cde () {
+		Debug.Log ("Vaibhv2");
+	}
+}
--- Assets/Scripts/DropDwn.cs
+++ Assets/Scripts/DropDwn.cs
@@ -17,26 +17,30 @@ public class DropDwn : MonoBehaviour {
 		if (GUI.Button (new Rect (25, 50, 200, 25), countrys[wichcountry])) {
 
 		}
-		GUI.Label(new Rect(30,75,300,25), "Destination");
+		GUI.Label(new Rect(Screen.width-250,25,300,25), "Destination");
 
-		if(GUI.Button(new Rect(25,100,200,25), "")){
+		if(GUI.Button(new Rect(Screen.width-250,50,200,25), "")){
 			if(n==0)n=1;
 			else n=0;        
 		}
 		
 		if(n==1){
-			scrollViewVector = GUI.BeginScrollView (new Rect (25, 125, 200, 200), scrollViewVector, new Rect (0, 0, 200, 200));
+			scrollViewVector = GUI.BeginScrollView (new Rect (Screen.width-250, 75, 200, 200), scrollViewVector, new Rect (0, 0, 200, 200));
 			GUI.Box(new Rect(0,0,300,500), ""); 
 			for(i=0;i<4;i++){
-				if(GUI.Button(new Rect(0,i*25,300,25), countrys[i])){
+				if(GUI.Button(new Rect(0,i*25,300,25), "")){
 					n=0;wichcountry=i;        
 				}              
-//				GUI.Label(new Rect(5,i*25,300,25), countrys[i]);           
+				GUI.Label(new Rect(5,i*25,300,25), countrys[i]);           
 			}
 			GUI.EndScrollView();        
 		}else{
 //			GUI.Label(new Rect(30,50,300,25), countrys[wichcountry]);
 		}
-		GUI.Label(new Rect(30,100,300,25), countrys[wichcountry]);
+		GUI.Label(new Rect(Screen.width-250,50,300,25), countrys[wichcountry]);
+
+//		if(GUI.Button (new Rect(Screen.width-250, Screen.height-250, 100, 100), "GO")) {
+//			Camera.main.gameObject.GetComponent<iTweenEvent> ().p
+//		}
 	}
 }
