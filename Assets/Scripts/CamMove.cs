3df1b561484c41bf98ea4b04701eb005424d8935
Vaibhav <faichi@Suryakants-MacBook-Pro.local>
2014-10-09 07:01:40 +0530

    test

--- Assets/Scripts/CamMove.cs
+++ Assets/Scripts/CamMove.cs
@@ -15,15 +15,22 @@ public class CamMove : MonoBehaviour {
 
 	void OnGUI () {
 		if (GUI.Button (new Rect (Screen.width - 250, Screen.height - 250, 200, 100), "GO")) {
-			iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("StartPath1"), "easetype", "linear", "time",25, "islocal",true, "orientToPath",true, "lookTime", 0.2, "loopType", "none", "oncomplete", "cde"));
+			iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("pathUpstaircase1"), "easetype", "linear", "time",25, "islocal",true, "orientToPath",true, "lookTime", 0.2, "loopType", "none", "oncomplete", "cde"));
 		}
 	}
 
 	public void abc () {
 		Debug.Log ("Vaibhv");
+		iTween.
+			/
 	}
 
 	public void cde () {
 		Debug.Log ("Vaibhv2");
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("pathUpstaircase2"), "easetype", "linear", "time",25, "islocal",true, "orientToPath",true, "lookTime", 0.2, "loopType", "none", "oncomplete", "endPath"));
+	}
+
+	public void endPath () {
+		Debug.Log ("Vaibhv3");
 	}
 }
